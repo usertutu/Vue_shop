f7c065e7623362549085dacb6e194951c3ef0eb0
usertutu <484027291@qq.com>
2021-01-27 22:50:43 +0800

    day2

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, 
+    Form, FormItem, Input, Message,
+    container,Header,Aside,
+    Main,Menu,Submenu,MenuItem,
+    breadcrumb,BreadcrumbItem,
+    Card,Row,Col,Table,TableColumn,
+    Switch,Tooltip } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
 Vue.prototype.$message = Message
